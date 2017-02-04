44521bb15bf31f1347d3efb067a8fbba304374b9
xman8293 <xman8293@163.com>
2017-02-04 17:53:01 +0800

    添加用户车辆页面

--- pages/ChooseCar/AddCar.js
+++ pages/ChooseCar/AddCar.js
@@ -10,21 +10,32 @@ Page({
     // 页面初始化 options为页面跳转所带来的参数
     var that = this;
     var id = options.id;
-    
+    var parm = {};
+    parm.carIds = id;
+    var url = app.AssembleUrl("basic/GetCar", parm);
 
+    wx.request({
+      url: url,
+      success: function (res) {
+        var car = res.data.data.list[0];
+        that.setData({
+          Cars: car
+        });
+      }
+    })
 
   },
   //添加用户车辆信息
   AddCar: function (event) {
-  var that = this;
-  
-   
+    var that = this;
+
+
   },
   //显示汽车其他属性的block 点击展开 点击隐藏
   widgetsToggle: function (e) {
     console.log(this.data.open);
-    var curopen =!this.data.open;
-     console.log(curopen);
+    var curopen = !this.data.open;
+    console.log(curopen);
     this.setData({
       open: curopen
     });
